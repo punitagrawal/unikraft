58a33d0e80a61ada9a033adaee5b8154f4547254
Punit Agrawal <punitagrawal@gmail.com>
2020-09-09 21:47:10 +0900

    driver/pci: sample: Create devfs node
    Add support to the sample driver to create a character device
    (/dev/dummy). The character device can be used by userspace
    application to interact with the sample driver.
    
    Signed-off-by: Punit Agrawal <punitagrawal@gmail.com>

--- plat/drivers/pci/sample.c
+++ plat/drivers/pci/sample.c
@@ -7,6 +7,7 @@
  *
  */
 
+#include <devfs/device.h>
 #include <pci/pci_bus.h>
 #include <uk/alloc.h>
 #include <uk/arch/types.h>
@@ -15,16 +16,60 @@
 #define SAMPLE_VENDOR_ID 0x1CAB
 #define SAMPLE_DEVICE_ID 0x4D0E
 
+#define DEV_DUMMY_NAME "dummy"
+
 struct sample_pci_dev {
 	struct pci_device *pdev;
+	struct device *dev;
 	__u16 base_addr;
 };
 
 static struct uk_alloc *allocator;
 
+static int dummy_open_op(struct device *device __unused, int mode __unused)
+{
+	return 0;
+}
+
+
+static int dummy_close_op(struct device *device __unused)
+{
+	return 0;
+}
+
+static int dummy_read_op(struct device *dev __unused, struct uio *uio __unused,
+			 int flags __unused)
+{
+	uk_pr_info("Reading from dummy device\n");
+	return 0;
+}
+
+static int dummy_write_op(struct device *dev __unused, struct uio *uio,
+	int flags __unused)
+{
+	uio->uio_resid = 0;
+	uk_pr_info("Writing to dummy device\n");
+
+	return 0;
+}
+
+static struct devops dummy_devops = {
+	.open  = dummy_open_op,
+	.close = dummy_close_op,
+	.read  = dummy_read_op,
+	.write = dummy_write_op,
+};
+
+static struct driver drv_dummy = {
+	.devops = &dummy_devops,
+	.devsz  = 0,
+	.name   = DEV_DUMMY_NAME,
+};
+
 static int sample_pci_add_dev(struct pci_device *pci_dev)
 {
 	struct sample_pci_dev *sdev;
+	struct device *dev;
 
 	sdev = uk_malloc(allocator, sizeof(*sdev));
 	if (!sdev) {
@@ -35,8 +80,19 @@ static int sample_pci_add_dev(struct pci_device *pci_dev)
 	sdev->pdev = pci_dev;
 	sdev->base_addr = pci_dev->base;
 
+	dev = device_create(&drv_dummy, DEV_DUMMY_NAME, D_CHR);
+	if (!dev) {
+		uk_pr_err("Failed to allocate devfs device\n");
+		goto dev_err;
+	}
+	sdev->dev = dev;
+
 	uk_pr_info("Registerd sample device\n");
 	return 0;
+
+dev_err:
+	uk_free(allocator, sdev);
+	return -ENOMEM;
 }
 
 static int sample_pci_drv_init(struct uk_alloc *drv_allocator)
